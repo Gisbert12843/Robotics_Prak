46bf57ad23eb0a9dbbe170e8f7d451e7c78ee40e
Kai Oestreich <kai.oestreich@lumni.fh-aachen.de>
2024-10-29 16:38:36 +0100

    Split Functions into Parts

--- prak2_ws/src/prak2_pkg/prak2_pkg/line_following_node.py
+++ prak2_ws/src/prak2_pkg/prak2_pkg/line_following_node.py
@@ -18,51 +18,64 @@ class LineFollower(Node):
 
 
 
-
   def convert_to_hsv_colorspace(self, image):
       original = image.copy()
       image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+      return image, original
+  
+  def filter_red_from_hsv_image(self, image):
+      
+
+      red_lower_hsv = numpy.array([0, 130, 130], dtype="uint8")
+      red_upper_hsv = numpy.array([10, 255, 255], dtype="uint8")
+      red_mask0 = cv2.inRange(image, red_lower_hsv, red_upper_hsv)
+
+      red_lower_hsv = numpy.array([170, 130, 130], dtype="uint8")
+      red_upper_hsv = numpy.array([180, 255, 255], dtype="uint8")
+      red_mask1 = cv2.inRange(image, red_lower_hsv, red_upper_hsv)
+
+      red_mask = red_mask0 + red_mask1
+
+      return red_mask
+  
+  def filter_yellow_from_hsv_image(self, image):
+        
       yellow_lower_hsv = numpy.array([20, 100, 100], dtype="uint8")
       yellow_upper_hsv = numpy.array([30, 255, 255], dtype="uint8")
       yellow_mask = cv2.inRange(image, yellow_lower_hsv, yellow_upper_hsv)
 
-      red_lower_hsv = numpy.array([170, 100, 0], dtype="uint8")
-      red_upper_hsv = numpy.array([180, 255, 255], dtype="uint8")
-      red_mask = cv2.inRange(image, red_lower_hsv, red_upper_hsv)
+      return yellow_mask
 
+  def mark_yellow_contours(self, image, yellow_mask):
+      
       yellow_contours = cv2.findContours(yellow_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
       yellow_contours = yellow_contours[0] if len(yellow_contours) == 2 else yellow_contours[1]
-      red_contours = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-      red_contours = red_contours[0] if len(red_contours) == 2 else red_contours[1]
 
       for c in yellow_contours:
           x,y,w,h = cv2.boundingRect(c)
-          print("h*w: "+str(h*w)+" x,y :"+str(x)+" "+str(y)) #debug printout - can be removed
-          cv2.rectangle(original, (x, y), (x + w, y + h), (36,255,12), 2)
+          # print("h*w: "+str(h*w)+" x,y :"+str(x)+" "+str(y)) #debug printout - can be removed
+          cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+
+
+  def mark_red_contours(self, image, red_mask):
+      red_contours = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+      red_contours = red_contours[0] if len(red_contours) == 2 else red_contours[1]
 
       for c in red_contours:
           x,y,w,h = cv2.boundingRect(c) 
-          print("h*w: "+str(h*w)+" x,y :"+str(x)+" "+str(y)) #debug printout - can be removed
-          cv2.rectangle(original, (x, y), (x + w, y + h), (36,255,12), 2)
+          # print("h*w: "+str(h*w)+" x,y :"+str(x)+" "+str(y)) #debug printout - can be removed
+          cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
 
-      return original
-      # cv2.imshow('yellow_mask', yellow_mask)
-      # cv2.imshow('red_mask', red_mask)
-      # cv2.imshow('original', original)
-      # cv2.waitKey()
-
-
-
-
-
+  def send_cmd():
+      
 
 
   def camera_cb(self, msg):
       #convert from ROS msg to OpenCV image
-      spot_gripper_img = CvBridge().imgmsg_to_cv2(msg, desired_encoding='bgr8')
+      spot_gripper_img = CvBridge().imgmsg_to_cv2(msg, desired_encoding='bgr8') #bgr8 is OpenCV’s default color format
       hsv_converted_image = self.convert_to_hsv_colorspace(spot_gripper_img)
-      cv2.imshow("Test Window", hsv_converted_image)
+      cv2.imshow("Test Window", hsv_converted_image[2])
       cv2.waitKey(1)
 
 
